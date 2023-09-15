50dbe32107afa4da21ecb056d600ab057f493622
AzizKri <aziz.kanakri@gmail.com>
2023-09-15 15:04:40 +0400

    Create Team entity

---
+++ server/src/entity/Team.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryColumn, OneToMany, Column, OneToOne, JoinColumn } from "typeorm"
+import { Student } from "./Student"
+import { Supervisor } from "./Supervisor"
+
+@Entity()
+export class Team {
+
+    @PrimaryColumn()
+    id: string
+
+    @Column()
+    school: string
+
+    @OneToMany(() => Student, (student) => student.team)
+    members: Student[]
+
+    @OneToOne(() => Supervisor)
+    @JoinColumn()
+    supervisor: Supervisor
+}
