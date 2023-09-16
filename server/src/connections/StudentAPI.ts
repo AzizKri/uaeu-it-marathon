92f0224f01644bb12cda9b90030c4274f95aee3e
AzizKri <aziz.kanakri@gmail.com>
2023-09-16 13:51:39 +0400

    Use promises & better error handling

--- server/src/connections/StudentAPI.ts
+++ server/src/connections/StudentAPI.ts
@@ -6,31 +6,49 @@ import { Team } from "../entity/Team";
 export async function getStudent(req: Request, res: Response) {
     if (req.params.sid === null) {
         return res.status(400).json({ error: "INCOMPLETE"});
-    }
+    };
     const sid = req.params.sid;
-    const student = await AppDataSource.getRepository(Student).findOne({
-        relations: {
-            team: true,
-        },
-        where: {
-            sid: sid,
-        }
-    });
-    if (student) {
-        return res.status(200).send(student);
-    } else return res.status(404);
-}
+    const getEntity = async () => {
+        const student = await AppDataSource.getRepository(Student).findOneOrFail({
+            relations: {
+                team: true,
+            },
+            where: {
+                sid: sid,
+            }
+        }).then(() => {
+            return res.status(200).send(student);
+        }).catch((e) => {
+            return res.status(404).send(e)
+        });
+    };
+    getEntity();
+};
 
 export async function createStudent(req: Request, res: Response) {
     const body = req.body;
-    if (!body || !body.sid || !body.fname || !body.lname || !body.school || !body.teamId) {
-        return res.status(400).json({error: "Missing information"})
+    if (!body || !body.sid || !body.fname || !body.lname || !body.school) {
+        return res.status(400).json({error: "INCOMPLETE"})
+    };
+    if (await AppDataSource.getRepository(Student).findOneBy({sid: body.sid})) {
+        return res.status(400).json({error: "ALREADY_EXIST"})
+    };
+    
+    if (body.teamId) {
+        const getTeam = async () => {
+            const team = AppDataSource.getRepository(Team).findOneByOrFail({
+                id: body.teamId
+            }).then(() => {
+                body.team = team;
+                body.teamId = null;
+            }).catch((e) => {
+                return res.status(404).send(e)
+            });
+        }
+        getTeam();
     }
-    const team = AppDataSource.getRepository(Team).findOneBy({id: body.teamId});
-    body.team = team;
-    body.teamId = null;
 
-    const student = AppDataSource.getRepository(Student).create(req.body);
+    const student = AppDataSource.getRepository(Student).create(body);
     const result = await AppDataSource.getRepository(Student).save(student);
     return res.status(200).send(result);
 }
