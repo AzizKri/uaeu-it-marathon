b26f678cbd2ba142c9732ac924101cb56e3c4bdf
AzizKri <aziz.kanakri@gmail.com>
2023-09-16 13:53:42 +0400

    Add Supervisor entity & use dotenv

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,20 +1,24 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
+import "reflect-metadata";
+import "dotenv/config";
+import { DataSource } from "typeorm";
 
-import { Student } from "./entity/Student"
-import { Team } from "./entity/Team"
+import { Student } from "./entity/Student";
+import { Team } from "./entity/Team";
+import { Supervisor } from "./entity/Supervisor";
+
+const { POSTGRES_DB_DEV, POSTGRES_HOST_DEV, POSTGRES_USER_DEV, POSTGRES_PASSWORD_DEV } = process.env;
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.POSTGRES_HOST_DEV,
+    host: POSTGRES_HOST_DEV,
     port: 5432,
-    username: process.env.POSTGRES_USER_DEV,
-    password: process.env.POSTGRES_PASSWORD_DEV,
-    database: process.env.POSTGRES_DB_DEV,
+    username: POSTGRES_USER_DEV,
+    password: POSTGRES_PASSWORD_DEV,
+    database: POSTGRES_DB_DEV,
     synchronize: true,
     ssl: true,
     logging: false,
-    entities: [Student, Team],
+    entities: [Student, Team, Supervisor],
     migrations: [],
     subscribers: [],
-})
+});
