8a8502b6923e0e161188866b863d4d75827ac71a
AzizKri <aziz.kanakri@gmail.com>
2023-10-14 18:18:34 +0400

    Add footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <Student />
+      <div className='Powered'>
+        <p><b>Powered by</b></p>
+        <img src="/cs.png" alt='CS Club'/>
+      </div>
     </div>
   );
 }
