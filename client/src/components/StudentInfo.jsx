6caed342a856824686c197233e3d654943a41ea2
Mohammed Naji <mohammadnaji126@gmail.com>
2023-10-14 17:50:54 +0400

    Added design for the card

--- client/src/components/StudentInfo.jsx
+++ client/src/components/StudentInfo.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
+import "./studentinfo.css";
 
 function StudentInfo({ title, name, school, team, supervisor }) {
     return (
-        <div>
-            Title: {title}
-            <br />
-            <br />
-            Name: {name}
-            <br />
-            <br />
-            School: {school}
-            <br />
-            <br />
-            Team: {team}
-            <br />
-            <br />
-            {(supervisor)? supervisor :""}
-            <br />
-            <br />
+        <div class="container">
+            <div class="padding">
+                <div class="font">
+                    <div class="top">
+                        <img src="%PUBLIC_URL%/download.png" />
+                    </div>
+                    <div class="bottom">
+                        <p>Student</p>
+                        <p class="desi">IT Marathon</p>
+                        <div class="barcode">
+                            <img src="%PUBLIC_URL%/qrsample.png" />
+                        </div>
+                        <br />
+                    </div>
+                </div>
+            </div>
+            <div class="back">
+                <h1 class="Details">information</h1>
+                <hr class="hr" />
+                <div class="details-info">
+                    <p><b class="name-field">Name : </b></p>
+                    <p class="name">Mohammad</p>
+                    <p class="supervisor-field"><b>Supervisor: </b></p>
+                    <p class="supervisor">Ali</p>
+                    <p class="school-field"><b>School:</b></p>
+                    <p class="school">FLPS</p>
+                </div>
+            </div>
+            <hr />
         </div>
     );
 }
