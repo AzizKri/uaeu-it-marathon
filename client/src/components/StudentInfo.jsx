1f8d0f4da78bb2813386699a8f89147dbade53f7
AzizKri <aziz.kanakri@gmail.com>
2023-10-14 18:19:34 +0400

    Final changes

--- client/src/components/StudentInfo.jsx
+++ client/src/components/StudentInfo.jsx
@@ -3,32 +3,34 @@ import "./studentinfo.css";
 
 function StudentInfo({ title, name, school, team, supervisor }) {
     return (
-        <div class="container">
-            <div class="padding">
-                <div class="font">
-                    <div class="top">
-                        <img src="%PUBLIC_URL%/download.png" />
+        <div className="container">
+            <div className="padding">
+                <div className="font">
+                    <div className="top">
+                        <img src="/download.png" alt='student'/>
                     </div>
-                    <div class="bottom">
-                        <p>Student</p>
-                        <p class="desi">IT Marathon</p>
-                        <div class="barcode">
-                            <img src="%PUBLIC_URL%/qrsample.png" />
+                    <div className="bottom">
+                        <p>{title}</p>
+                        <p className="desi">IT Marathon</p>
+                        <div className="barcode">
+                            <img src="/qrsample.png" alt='qr code'/>
                         </div>
                         <br />
                     </div>
                 </div>
             </div>
-            <div class="back">
-                <h1 class="Details">information</h1>
-                <hr class="hr" />
-                <div class="details-info">
-                    <p><b class="name-field">Name : </b></p>
-                    <p class="name">Mohammad</p>
-                    <p class="supervisor-field"><b>Supervisor: </b></p>
-                    <p class="supervisor">Ali</p>
-                    <p class="school-field"><b>School:</b></p>
-                    <p class="school">FLPS</p>
+            <div className="back">
+                <h1 className="Details">Information</h1>
+                <hr className="hr" />
+                <div className="details-info">
+                    <p><b className="name-field">Name: </b></p>
+                    <p className="name">{name}</p>
+                    <p className="school-field"><b>School:</b></p>
+                    <p className="school">{school}</p>
+                    <p className="supervisor-field"><b>Team: </b></p>
+                    <p className="supervisor">{team}</p>
+                    <p className="supervisor-field"><b>Supervisor: </b></p>
+                    <p className="supervisor">{(supervisor)? supervisor : ""}</p>
                 </div>
             </div>
             <hr />
