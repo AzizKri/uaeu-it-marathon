f36c1b62c94cb767da8f8cbdb10fec035f8a3b4e
AzizKri <aziz.kanakri@gmail.com>
2023-11-02 13:23:12 +0400

    Update imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Student from './containers/Student';
+import InfoPage from './containers/InfoPage';
 
 function App() {
   return (
     <div className="App">
-      <Student />
+      <InfoPage />
     </div>
   );
 }
