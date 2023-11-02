3baf9b26bdc72ebee5e8eb6cb32bf213f4bc4a4b
AzizKri <aziz.kanakri@gmail.com>
2023-11-02 13:23:41 +0400

    Better export

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Loading } from './Loading';
+export { default as NotFound } from './NotFound';
+export { default as Redirect } from './Redirect';
+export { default as StudentInfo } from './StudentInfo';
+export { default as TeamInfo } from './TeamInfo';
