ca9d9f87a1b3ba4e27f197f6564cb99ba45d6c83
AzizKri <aziz.kanakri@gmail.com>
2023-11-02 13:27:10 +0400

    Split into components

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Loading() {
+    return (
+		<div className="not_found">
+			<h1>Loading...</h1>
+			<div className='Powered'>
+				<p><b>Powered by</b></p>
+				<img src="/cs.png" alt='CS Club'/>
+			</div>
+		</div>
+    );
+}
+
+export default Loading;
