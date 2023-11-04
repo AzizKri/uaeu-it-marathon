fc7300ba01a25f6b600aa89daf23d31524c2fc48
AzizKri <aziz.kanakri@gmail.com>
2023-11-04 20:10:40 +0400

    Format team info

--- client/src/components/TeamInfo.jsx
+++ client/src/components/TeamInfo.jsx
@@ -3,51 +3,53 @@ import "./teaminfo.css";
 
 function TeamInfo({ team_id, team, school, supervisor, students }) {
     return (
-        <div>
-            <div className="container">
-                <div className="padding">
-                    <div className="font">
-                        <div className="top">
-                            <img src="/download.png" alt='student'/>
-                        </div>
-                        <div className="bottom">
-                            <p>{team}</p>
-                            <p className="desi">IT Marathon</p>
-                            <div className="barcode">
-                                <img src="/logo256.png" alt='itm logo'/>
-                            </div>
-                            <br />
-                        </div>
-                    </div>
-                </div>
-                <div className="back">
-                    <h1 className="Details">Information</h1>
-                    <hr className="hr" />
-                    <div className="details-info">
-                        <p><b className="name-field">Team {team_id}</b></p>
-                        <p className="school-field"><b>School:</b></p>
-                        <p className="school">{school}</p>
-                        <p className="supervisor-field"><b>Team: </b></p>
-                        <p className="supervisor">{team}</p>
-                        <p className="supervisor-field"><b>{(supervisor.length <= 2)? "" : "Supervisor: "}</b></p>
-                        <p className="supervisor">{supervisor}</p>
-                        <p className="student-field"><b>Students: </b></p>
-                        <p className="student">{students[0]}</p>
-                        <p className="student">{students[1]}</p>
-						{(students[2] === undefined || students[2].length <= 2)? 
-						<></> : <p className="student">{students[2]}</p>}
-						{(students[3] === undefined || students[3].length <= 2)? 
-						<></> : <p className="student">{students[3]}</p>}
-						
-                    </div>
-                </div>
-                <hr />
-            </div>
+		<div>
+			<div className="team_container">
+				<div className="team_padding">
+					<div className="team_font">
+						<div className="team_top">
+							<h1 className="team_Details">ID: {team_id}</h1>
+							<hr /> 
+						</div>
+						<div className="team_bottom">
+							<p>Team:</p>
+							<p className="team_info">{team}</p>
+							<br></br>
+							<p>School:</p>
+							<p className="team_info">{school}</p>
+							<br></br>
+							<p>Supervisor:</p>
+							<p className="team_info">{supervisor}</p>
+							<br></br>
+						</div>
+					</div>
+				</div>
+				<div className="team_back">
+					<h1 className="team_Details">Students</h1>
+					<hr className="team_hr"/>
+					<div className="team_details-info">
+						<p className="team_name-field">{students[0]}</p>
+						<p className="team_name-field">
+						<hr className='team_student-separator'/>{students[1]}</p>
+						{(students[2] !== undefined && students[2].length >= 2)?
+						<p className="team_name-field">
+						<hr className='team_student-separator'/>
+						{students[2]}</p> :
+						<></>}
+						{(students[3] !== undefined && students[3].length >= 2)?
+						<p className="team_name-field">
+						<hr className='team_student-separator'/>
+						{students[3]}</p> :
+						<></>}
+						</div>
+				</div>
+				<hr />
+			</div>
             <div className='Info_Powered'>
                 <p><b>Powered by</b></p>
                 <img src="/cs.png" alt='CS Club'/>
             </div>
-        </div>
+		</div>
     );
 }
 
